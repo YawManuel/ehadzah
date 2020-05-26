e247f49cb39e79f51ca4d16211bd7928b1551cef
Manuel <yawmanuelfestive@gmail.com>
2020-05-26 11:40:32 -0700

    Initial commit

---
+++ js/toggle.js
@@ -0,0 +1,45 @@
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if(!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach(item => item.classList.add('open'));
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach(item => item.classList.remove('open'));
+
+    showMenu = false;
+  }
+}
+
+//     var coll = document.getElementsByClassName("collapsiblee");
+//     var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
+
