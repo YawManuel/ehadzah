e6e0c4bdbc1f88a7bc592054b1ed4fb1b2eed457
Manuel <yawmanuelfestive@gmail.com>
2020-07-06 22:12:40 -0700

    beta test

--- js/toggle.js
+++ js/toggle.js
@@ -28,18 +28,3 @@ function toggleMenu() {
   }
 }
 
-//     var coll = document.getElementsByClassName("collapsiblee");
-//     var i;
-
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
-// }
-
