71fff3c965b19d1dc3986306dee3c8474c2d94c6
Manuel <yawmanuelfestive@gmail.com>
2020-10-26 12:25:35 -0700

    apple init commit

---
+++ vue/vuereadlink.js
@@ -0,0 +1,8 @@
+Vue.component('vuereadlink', {
+    template: 
+    `<a href="./read/The-New-Apples-From-Steve-Jobs-Farm.html"> Read </a>`    
+});
+
+new Vue({
+    el:'#vuelink'
+});
---
+++ vue/vuetopread.js
@@ -0,0 +1,23 @@
+Vue.component('vuetopread', {
+    template: 
+    `<div class="spanhr">TOP READ</div> <hr>
+
+<article class="yartrend">
+
+<a href="../read/The-Future-of-Reality-AR-and-VR.html" margin-top="0">
+    <figure>
+        <Img src="../assets/img/img-read/realitypic.jpg"/>
+        <h2>The Future of Reality: AR and VR</h2>
+    </figure>
+</a>
+<a href="../read/The-New-Internet-Starlink-Connecting-the-Stars.html">
+    <figure>
+        <Img src="../assets/img/img-read/starlinkmainpic.jpg"/>
+        <h2>The New Internet: Starlink, connecting the stars</h2>
+    </figure>
+</a>`
+});
+
+new Vue({
+    el:'#topread'
+});
